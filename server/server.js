dd4aff80d4558e44e9f257458e318251131af6f0
Rusty12321 <weston@hilgenfeld.org>
2023-05-04 15:09:56 -0500

    imported dependencies and updated get route

--- server/server.js
+++ server/server.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const pg = require("pg");
+const { Pool } = pg;
+const pool = new Pool({connectionString: process.env.DATABASE_URL});
+pool.connect();
+
 app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.send('Hello World');
+app.get('/api/discs', (req, res) => {
+    pool.query("SELECT * FROM disc").then((result) => {
+        res.send(result.rows);
+    })
 })
 
 app.listen(port, () => {
