33b6cec9f0bec6b1941788eb47965acdabfb7cb7
Rusty12321 <weston@hilgenfeld.org>
2023-05-08 18:22:45 -0500

    added external api

--- client/src/addForm.jsx
+++ client/src/addForm.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Data from "./data";
 
 const addForm = (props) => {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState(Data());
+  const searchBar = document.querySelector("#searchBar");
+  const dropDown = document.querySelector(".dropDown");
+  const nameEl = document.getElementById("name");
+  const plasticTypeEl = document.getElementById("plastic-type");
+  const discTypeEl = document.getElementById("disc-type");
+  const colorEl = document.getElementById("color");
+  const weightEl = document.getElementById("weight");
+  const speedEl = document.getElementById("speed");
+  const glideEl = document.getElementById("glide");
+  const turnEl = document.getElementById("turn");
+  const fadeEl = document.getElementById("fade");
+  const manufacturerEl = document.getElementById("manufacturer");
+
   function addOrEditDisc() {
     if (!props.addOrEdit) {
-      fetch("http://localhost:3000/api/discs", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(getValues()),
-      }).then(() => {
-        props.setPage("main");
-        props.getBag();
-        props.getStorage();
-      });
+      if (validInputs()) {
+        fetch("http://localhost:3000/api/discs", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(getValues()),
+        }).then(() => {
+          props.setPage("main");
+          props.getBag();
+          props.getStorage();
+        });
+      }
     } else {
       fetch(`http://localhost:3000/api/discs/${props.addOrEdit}`, {
         method: "PATCH",
@@ -29,21 +47,55 @@ const addForm = (props) => {
     }
   }
 
+  function validInputs() {
+    let isValid = true;
+    let inputs = document.querySelectorAll(".add-inputs");
+    inputs.forEach((input) => {
+      if (!input.value) {
+        input.placeholder = "Please Fill Out This Field!";
+        isValid = false;
+      }
+    });
+    return isValid;
+  }
+
   function getValues() {
     return {
-      name: document.getElementById("name").value,
-      type_of_plastic: document.getElementById("plastic-type").value,
-      type_of_disc: document.getElementById("disc-type").value,
-      color: document.getElementById("color").value,
-      weight_in_grams: document.getElementById("weight").value,
-      speed: document.getElementById("speed").value,
-      glide: document.getElementById("glide").value,
-      turn: document.getElementById("turn").value,
-      fade: document.getElementById("fade").value,
-      company_name: document.getElementById("manufacturer").value,
+      name: nameEl.value,
+      type_of_plastic: plasticTypeEl.value,
+      type_of_disc: discTypeEl.value,
+      color: colorEl.value,
+      weight_in_grams: weightEl.value,
+      speed: speedEl.value,
+      glide: glideEl.value,
+      turn: turnEl.value,
+      fade: fadeEl.value,
+      company_name: manufacturerEl.value,
       bag_or_storage: props.page,
     };
+  }  
+
+  function handleClick(name) {
+    fetch(`https://discit-api.fly.dev/disc/${name}`)
+      .then((result) => result.json())
+      .then((data) => {
+        nameEl.value = data.name;
+        discTypeEl.value = data.category;
+        speedEl.value = data.speed;
+        glideEl.value = data.glide;
+        turnEl.value = data.turn;
+        fadeEl.value = data.fade;
+        manufacturerEl.value = data.brand;
+      });
   }
+
+  document.addEventListener("click", function (e) {
+    if (e.target !== searchBar) {
+      dropDown.style.display = "none";
+    } else {
+      dropDown.style.display = "block";
+    }
+  });
 
   if (props.addOrEdit) {
     fetch(`http://localhost:3000/api/discs/${props.addOrEdit}`)
@@ -64,21 +116,53 @@ const addForm = (props) => {
 
   return (
     <div>
-      <button
-        id="homeBtn"
-        className="button"
-        onClick={() => props.setPage("main")}
-      >
-        Home
-      </button>
-      <div className="addForm">
-        <div>
-          <div>Manufacturer:</div>
-          <input type="text" id="manufacturer" className="add-inputs" />
+      <div className="navBar">
+        <button
+          id="homeBtn"
+          className="button"
+          onClick={() => props.setPage("main")}
+        >
+          Home
+        </button>
+        <div className="searchContainer">
+          <input
+            type="text"
+            id="searchBar"
+            placeholder="Enter Disc Name"
+            autoComplete="off"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <div className="dropDown">
+            {data
+              .filter((disc) => {
+                if (query === "") {
+                  return;
+                } else if (
+                  disc.name.toLowerCase().includes(query.toLowerCase())
+                ) {
+                  return disc;
+                }
+              })
+              .map((disc) => (
+                <div
+                  key={disc.id}
+                  className="searchedDisc"
+                  onClick={() => handleClick(disc.name.toLowerCase())}
+                >
+                  <p>{disc.name}</p>
+                </div>
+              ))}
+          </div>
         </div>
+      </div>
+      <div className="addForm">
         <div>
           <div>Disc Name:</div>
           <input type="text" id="name" className="add-inputs" />
+        </div>
+        <div>
+          <div>Manufacturer:</div>
+          <input type="text" id="manufacturer" className="add-inputs" />
         </div>
         <div>
           <div>Type of Plastic:</div>
---
+++ client/src/data.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const data =() => {
+    let discs = [];
+    fetch("https://discit-api.fly.dev/disc")
+        .then(result => result.json())
+        .then(data => {
+            data.map((disc) => {
+                discs.push({name: disc.name})
+            })
+        })
+    return discs;
+}
+
+export default data;
